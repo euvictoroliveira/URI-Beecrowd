e47b3cb3c09f491a86aa74cc444712fe8d434259
Victor Oliveira <victoroliver249@gmail.com>
2025-12-01 22:35:52 -0300

    add: beecrowd2926 in c++

---
+++ ad-hoc/C++/beecrowd2926.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    int I;
+
+    cin >> I;
+
+    cout << "Ent";
+    for(int i = 0; i < I; i++)
+        cout << "a";
+    cout << "o eh N";
+    for(int i = 0; i < I; i++)
+        cout << "a";
+    cout << "t";
+    for(int i = 0; i < I; i++)
+        cout << "a";
+    cout << "l!" << endl;
+
+
+    return 0;
+}

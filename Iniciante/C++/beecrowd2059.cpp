d4373028f7f17114e4baf3aed00c689af3db74e2
Victor Oliveira <victoroliver249@gmail.com>
2025-12-02 21:52:29 -0300

    add: beecrowd2059 in c++

---
+++ Iniciante/C++/beecrowd2059.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    int p, j1, j2, r, a;
+
+    cin >> p >> j1 >> j2 >> r >> a;
+
+    int soma = j1 + j2;
+    if( p == 1){
+
+        if(soma % 2 == 0){
+            if(r == a && a == 0)
+                cout << "Jogador 1 ganha!" << endl;
+            else if(r == a && a == 1)
+                cout << "Jogador 2 ganha!" << endl;
+            else if(r == 1 && a == 0)
+                cout << "Jogador 1 ganha!" << endl;
+            else if(r == 0 && a == 1)
+                cout << "Jogador 1 ganha!" << endl;
+        }else{
+            if(r == a && a == 0)
+                cout << "Jogador 2 ganha!" << endl;
+            else if(r == a && a == 1)
+                cout << "Jogador 2 ganha!" << endl;
+            else if(r == 1 && a == 0)
+                cout << "Jogador 1 ganha!" << endl;
+            else if(r == 0 && a == 1)
+                cout << "Jogador 1 ganha!" << endl;
+        }
+
+    }else{
+        if(soma % 2 == 0){
+            if(r == a && a == 0)
+                cout << "Jogador 2 ganha!" << endl;
+            else if(r == a && a == 1)
+                cout << "Jogador 2 ganha!" << endl;
+            else if(r == 1 && a == 0)
+                cout << "Jogador 1 ganha!" << endl;
+            else if(r == 0 && a == 1)
+                cout << "Jogador 1 ganha!" << endl;
+        }else{
+            if(r == a && a == 0)
+                cout << "Jogador 1 ganha!" << endl;
+            else if(r == a && a == 1)
+                cout << "Jogador 2 ganha!" << endl;
+            else if(r == 1 && a == 0)
+                cout << "Jogador 1 ganha!" << endl;
+            else if(r == 0 && a == 1)
+                cout << "Jogador 1 ganha!" << endl;
+
+
+        }
+    }
+
+    return 0;
+
+}
+    

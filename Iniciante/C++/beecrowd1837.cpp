f1a3858d8b3edb599f836a3bb3fa88a70583dc50
Victor Oliveira <victoroliver249@gmail.com>
2025-12-22 23:20:03 -0300

    add: beecrowd1837 in c++

---
+++ Iniciante/C++/beecrowd1837.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int calcula(int a, int b){
+    int quociente;
+
+    if(a > 0){
+        if(b > 0){
+            quociente = a / b;
+        }else{
+            quociente = ceil((double) a / b);
+        }
+    }else{
+        if(b > 0){
+            quociente = floor((double) a / b);
+        }else{
+            quociente = ceil((double) a / b);
+        }
+
+    }
+    return quociente;
+}
+
+int main(){
+    int a, b;
+
+
+    cin >> a >> b;
+    int quociente = calcula(a,b);
+
+    cout << quociente << " " << a - (b * quociente) << endl;
+
+    return 0;
+}

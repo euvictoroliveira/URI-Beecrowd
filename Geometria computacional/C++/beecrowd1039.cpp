21f603a6ae2fc0f1d31b92528fa2a77e94149e50
Victor Oliveira <victoroliver249@gmail.com>
2025-12-23 23:42:43 -0300

    feat: adiciona categoria geometria computacional

---
+++ Geometria computacional/C++/beecrowd1039.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+double distancia(int x1, int y1, int x2, int y2)
+{
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+
+int main()
+{
+    int r1, x1, y1, r2, x2, y2;
+
+    while (cin >> r1 >> x1 >> y1 >> r2 >> x2 >> y2){
+        if (distancia(x1, y1, x2, y2) <= (r1 - r2))
+        {
+            cout << "RICO" << endl;
+        }
+        else
+        {
+            cout << "MORTO" << endl;
+        }
+    }
+
+    return 0;
+}

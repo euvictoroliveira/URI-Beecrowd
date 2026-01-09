c6e39ccc0a5651332b1fd96a8a96fd96f54c8033
Victor Oliveira <victoroliver249@gmail.com>
2026-01-08 23:53:15 -0300

    add: beecrowd1848 in c++

---
+++ Iniciante/C++/beecrowd1848.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int valor(char caractere){
+    switch(caractere){
+        case '-':   return 0;
+        case '*':   return 1;
+    }
+}
+
+int converte(string numero){
+    int potencia = 1, resposta = 0;
+    
+    for(int i = numero.length() - 1; i > -1; i--){
+        resposta += valor(numero[i]) * potencia;
+        potencia *= 2;
+    }
+    return resposta;
+}
+
+int main(){
+    int soma = 0;
+    string linha;
+
+    while (getline(cin, linha)){
+        if (linha == "caw caw"){
+            cout << soma << endl;
+            soma = 0;
+        }
+        else{
+            soma += converte(linha);
+        }
+    }
+
+    return 0;
+}
